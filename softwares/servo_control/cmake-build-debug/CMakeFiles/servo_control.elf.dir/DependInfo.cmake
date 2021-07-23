6a0cf9427509bc040c03f2f46317fddea1624b6d
Forone <forone.sj.zhang@outlook.com>
2021-07-23 09:17:28 +0800

    add a repo

---
+++ softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/DependInfo.cmake
@@ -0,0 +1,72 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "G:/Project/Food_Robot/softwares/servo_control/startup/startup_stm32f407xx.s" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/startup/startup_stm32f407xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "G:/Project/Food_Robot/softwares/servo_control/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Src/main.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Src/main.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Src/retarget.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Src/retarget.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Src/stm32f4xx_hal_msp.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Src/stm32f4xx_hal_msp.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Src/stm32f4xx_it.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Src/stm32f4xx_it.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Src/syscalls.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Src/syscalls.c.obj"
+  "G:/Project/Food_Robot/softwares/servo_control/Src/system_stm32f4xx.c" "G:/Project/Food_Robot/softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/Src/system_stm32f4xx.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ softwares/servo_control/cmake-build-debug/CMakeFiles/servo_control.elf.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Src/main.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Src/retarget.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Src/stm32f4xx_hal_msp.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Src/stm32f4xx_it.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Src/syscalls.c.obj"
+  "CMakeFiles/servo_control.elf.dir/Src/system_stm32f4xx.c.obj"
+  "CMakeFiles/servo_control.elf.dir/startup/startup_stm32f407xx.s.obj"
+  "servo_control.elf"
+  "servo_control.elf.manifest"
+  "servo_control.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/servo_control.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
